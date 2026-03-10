c400a738a6750945d5b11e233cd5ce949e3cb390
Akhil Pesala <khiliam.74@gmail.com>
2026-03-10 14:30:46 +0530

    q4

---
+++ lvl2/q4.java
@@ -0,0 +1,24 @@
+package lvl2;
+import java.util.Scanner;
+public class q4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } else {
+                    System.out.println(i + " is Odd");
+                }
+            }
+        } else {
+            System.out.println("The number is not a Natural Number.");
+        }
+
+        sc.close();
+    }
+}
