2b080e51ea118d350ed3f2678b130e0f16c84a57
Akhil Pesala <khiliam.74@gmail.com>
2026-03-17 13:49:56 +0530

    q3

---
+++ HelloApp/uc3.java
@@ -0,0 +1,12 @@
+package HelloApp;
+
+public class uc3 {
+    public static void main(String[] args) {
+
+        // Using ternary operator
+        String name = (args.length > 0) ? args[0] : "World";
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
+
