11a8027ef00963664c28a028710f3eafa23ccc93
Akhil Pesala <khiliam.74@gmail.com>
2026-03-25 13:06:37 +0530

    1

---
+++ uc3.java
@@ -0,0 +1,12 @@
+public class uc3 {
+    
+
+    public static void main(String[] args) {
+
+        // Using ternary operator
+        String name = (args.length > 0) ? args[0] : "World";
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
+
