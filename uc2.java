4225980ad98aed0630b5ddb6b66d3fc31c0d5630
Akhil Pesala <khiliam.74@gmail.com>
2026-03-25 13:05:03 +0530

    1

---
+++ uc2.java
@@ -0,0 +1,16 @@
+public class uc2 {
+    
+
+    public static void main(String[] args) {
+
+        // Check if at least one argument is provided
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Handle case when no argument is passed
+            System.out.println("Hello! Please provide your name as a command-line argument.");
+        }
+    }
+}
+
