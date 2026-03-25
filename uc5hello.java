8ebb7dfcaf80d14415f45bb4e795020bc40ef65d
Akhil Pesala <khiliam.74@gmail.com>
2026-03-25 13:43:16 +0530

    q5

---
+++ uc5hello.java
@@ -0,0 +1,25 @@
+public class uc5hello {
+    public static void main(String[] args) {
+
+        StringBuilder names = new StringBuilder();
+
+        if (args.length == 0) {
+            names.append("World");
+        } else {
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    names.append(", ");
+                }
+                names.append(name);
+                first = false;
+            }
+        }
+
+        System.out.println("Hello, " + names.toString() + "!");
+    }
+}
+
+
+
